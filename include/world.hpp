8bcd2b2c931f5c3beb440d8ef6dda023d71fb500
Toru Niina <niina.toru.68u@gmail.com>
2023-12-10 19:58:38 +0900

    fix: return nearest obj

--- include/world.hpp
+++ include/world.hpp
@@ -29,16 +29,20 @@ template<std::size_t N>
 constexpr std::optional<std::pair<collision_info, object>>
 collides(const ray& r, const world<N>& w, const double t_min, const double t_max) noexcept
 {
+    std::optional<std::pair<collision_info, object>> nearest = std::nullopt;
     for(const auto& object : w.objects)
     {
         if(const auto hit = std::visit([&r, t_min, t_max](const auto& obj) {
                 return collides(r, obj, t_min, t_max);
             }, object.shape))
         {
-            return std::make_pair(*hit, object);
+            if(hit->t < nearest.transform([](auto n){return n.first.t;}).value_or(t_max))
+            {
+                nearest = std::make_pair(*hit, object);
+            }
         }
     }
-    return std::nullopt;
+    return nearest;
 }
 
 } // world
